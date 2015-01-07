3bfa10405de4f3f068c3d1bc8a1cb74529016167
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-01-07 11:23:55 -0800

    Bootstrap an Xcode project and workspace

---
+++ OGDL/OGDL.h
@@ -0,0 +1,19 @@
+//
+//  OGDL.h
+//  OGDL
+//
+//  Created by Justin Spahr-Summers on 2015-01-07.
+//  Copyright (c) 2015 Carthage. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OGDL.
+FOUNDATION_EXPORT double OGDLVersionNumber;
+
+//! Project version string for OGDL.
+FOUNDATION_EXPORT const unsigned char OGDLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OGDL/PublicHeader.h>
+
+
