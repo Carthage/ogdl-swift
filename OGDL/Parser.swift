43ca575b813db76b1328fad0562fb7a4f8d72fa4
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2015-01-07 14:33:25 -0800

    Add a parser combinator for NSCharacterSet

---
+++ OGDL/Parser.swift
@@ -0,0 +1,25 @@
+//
+//  Parser.swift
+//  OGDL
+//
+//  Created by Justin Spahr-Summers on 2015-01-07.
+//  Copyright (c) 2015 Carthage. All rights reserved.
+//
+
+import Foundation
+import Madness
+
+/// Returns a parser which parses one character from the given set.
+internal prefix func % (characterSet: NSCharacterSet) -> Parser<String>.Function {
+	return { string in
+		let scalars = string.unicodeScalars
+
+		if let scalar = first(scalars) {
+			if characterSet.longCharacterIsMember(scalar.value) {
+				return (String(scalar), String(dropFirst(scalars)))
+			}
+		}
+
+		return nil
+	}
+}
