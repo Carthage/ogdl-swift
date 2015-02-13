830fbb5091b23d31e2875d6086f2cec32d16dfc7
Rob Rix <rob.rix@github.com>
2015-02-13 18:41:19 -0500

    Adjacent elements can be terminated with groups.

--- OGDL/Parser.swift
+++ OGDL/Parser.swift
@@ -122,8 +122,8 @@ private let descendents: Parser<Node>.Function = fix { descendents in descendent
 
 /// Parses a sequence of adjacent sibling elements, e.g.:
 ///
-///		x, y z, w # => [ Node(x), Node(y, Node(z)), Node(w) ]
-private let adjacent: Parser<[Node]>.Function = interleave(separator, descendents)
+///		x, y z, w (u, v) # => [ Node(x), Node(y, Node(z)), Node(w, [ Node(u), Node(v) ]) ]
+private let adjacent: Parser<[Node]>.Function = lazy { interleave(separator, descendents >>- { node in (optionalSpace ++ group) --> { node.byAppendingChildren($1) } }) }
 
 /// Parses a parenthesized sequence of sibling elements, e.g.:
 /// 
